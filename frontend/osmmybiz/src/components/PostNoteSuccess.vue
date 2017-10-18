71b2a886b4d794566b6f1282c1b68e8b6ad96978
simonheller94 <simonheller94@gmail.com>
2017-10-18 16:10:22 +0200

    add success component and very simple styling

---
+++ frontend/osmmybiz/src/components/PostNoteSuccess.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div>
+    Sind diese Informationen richtig?
+
+    <p>{{note.properties.comments[0].text}}</p>
+
+    <button class="button" @click="gotoLanding()">Alles richtig</button>
+    <button class="button" @click="gotoDetail()">Informationen anpassen</button>
+  </div>
+
+</template>
+
+<script>
+  import {mapGetters, mapActions, mapMutations} from 'vuex'
+
+  export default {
+    name: 'post-note-success',
+    computed: {
+      ...mapGetters([
+        'lon',
+        'lat',
+        'note',
+        'displaySuccess'
+      ])
+    },
+    methods: {
+      ...mapMutations([
+        'setDisplaySuccess'
+      ]),
+      ...mapActions([
+        'postNote'
+      ]),
+      gotoLanding () {
+        this.$router.push('/')
+      },
+      gotoDetail () {
+        this.setDisplaySuccess(false)
+        this.$router.push('/detail')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
