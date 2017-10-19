58b7d07f7cc12ceeb3605f58fdc7a43784f9b257
Max Luethi <m1luethi@hsr.ch>
2017-10-19 07:13:29 +0200

    fix db port

--- backend/api/app/__init__.py
+++ backend/api/app/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://osm-my-biz:123456@database:54321/osm-my-biz'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://osm-my-biz:123456@database:5432/osm-my-biz'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
