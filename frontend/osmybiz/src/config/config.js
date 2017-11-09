e6215311420dd8f101ccf62d14bec86217d32701
Max Luethi <m1luethi@hsr.ch>
2017-11-09 13:31:06 +0100

    added config file and moved mapboxaccesstoken into it

---
+++ frontend/osmybiz/src/config/config.js
@@ -0,0 +1,2 @@
+// accesstoken for satellite imagery of mapbox can be generated with mapbox account
+export const mapBoxToken = 'pk.eyJ1IjoibXRoaSIsImEiOiJjajlzZHJqZGc2bGRxMnhxbTd0bjVibDNjIn0.11MBq0_6S30JBIw7oo9O7A'
--- frontend/osmybiz/src/util/mapUtils.js
+++ frontend/osmybiz/src/util/mapUtils.js
@@ -1,7 +1,7 @@
 import * as L from 'leaflet'
+import {mapBoxToken} from '../config/config'
 
-const token = 'pk.eyJ1IjoibXRoaSIsImEiOiJjajlzZHJqZGc2bGRxMnhxbTd0bjVibDNjIn0.11MBq0_6S30JBIw7oo9O7A'
-const mapbox = 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.jpg70?access_token=' + token
+const mapbox = 'https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.jpg70?access_token=' + mapBoxToken
 const osm = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
 
 export const tileUrls = {
