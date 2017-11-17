08d6ef4a172fc7504f97760c41a431c852ac9834
simonheller94 <simonheller94@gmail.com>
2017-11-17 12:22:35 +0100

    change initialZoom to 8

--- frontend/osmybiz/src/config/config.js
+++ frontend/osmybiz/src/config/config.js
@@ -4,4 +4,4 @@ import {latLng} from 'leaflet'
 export const mapBoxToken = 'pk.eyJ1IjoibXRoaSIsImEiOiJjajlzZHJqZGc2bGRxMnhxbTd0bjVibDNjIn0.11MBq0_6S30JBIw7oo9O7A'
 
 export const initialPosition = latLng(46.87897, 8.23975) // aprox center of switzerland
-export const initialZoom = 11
+export const initialZoom = 8
