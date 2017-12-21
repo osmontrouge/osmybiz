e11be6d0bdc4fbbd6c544cf01c6328064d3bb344
Max Luethi <m1luethi@hsr.ch>
2017-12-21 14:06:33 +0100

    fix buttons in footer

--- frontend/osmybiz/src/components/FormFooter.vue
+++ frontend/osmybiz/src/components/FormFooter.vue
@@ -115,14 +115,16 @@
   .form-footer {
     position: fixed;
     height: 60px;
-    margin: 0 5% 0;
-    width: 90%;
+    margin: 0 auto;
+    max-width: 1000px;
     bottom: 0;
     background: white;
     display: flex;
     flex-direction: row;
     align-items: stretch;
     justify-content: flex-start;
+    left: 0;
+    right: 0;
   }
 
   .button {
