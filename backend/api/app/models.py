ffe2f2221b67ce4471e1aacac10dab7c58c82bd8
ANGkeith <kang024@e.ntu.edu.sg>
2018-11-27 17:16:18 +0800

    renaming osm_note_id in backend to note_id

--- backend/api/app/models.py
+++ backend/api/app/models.py
@@ -32,8 +32,8 @@ class BusinessPOI(db.Model):
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
     name = db.Column(db.String)
+    note_id = db.Column(db.BigInteger)
     osm_id = db.Column(db.BigInteger)
-    osm_note_id = db.Column(db.BigInteger)
     osm_type = db.Column(db.String(8))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     receive_updates = db.Column(db.Boolean)
@@ -44,14 +44,14 @@ class BusinessPOI(db.Model):
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
 
-    def __init__(self, user_id, lat, lng, name, osm_id, osm_note_id, osm_type,
+    def __init__(self, user_id, lat, lng, name, note_id, osm_id, osm_type,
                  receive_updates, version):
         self.user_id = user_id
         self.lat = lat
         self.lng = lng
         self.name = name
+        self.note_id = note_id
         self.osm_id = osm_id
-        self.osm_note_id = osm_note_id
         self.osm_type = osm_type
         self.receive_updates = receive_updates
         self.version = version
