b51c0473c0aa727336e5ede792e0ccc2a3b4c325
Seemann Benjamin <bseemann@hsr.ch>
2019-05-01 09:35:48 +0200

    started adding the OSMOpening hours converter.

--- frontend/osmybiz/src/components/detail/DetailForm.vue
+++ frontend/osmybiz/src/components/detail/DetailForm.vue
@@ -52,7 +52,7 @@
 			<input type="text"
 					v-model="details.opening_hours_url"
 					:placeholder="$t('detail.placeholders.opening_hours_url')"
-					onblur="console.log("Test")">
+					onblur="console.log('test')">
 		</div>
 		
         <div class="field">
