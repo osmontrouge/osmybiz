6a837d9c0f92b9c0b7cdc1bcd6532bd845003274
Seemann Benjamin <bseemann@hsr.ch>
2019-08-14 16:22:52 +0200

    Moved converter js into store

--- frontend/osmybiz/build/webpack.base.conf.js
+++ frontend/osmybiz/build/webpack.base.conf.js
@@ -44,7 +44,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('static/converter.js')]
+        include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
