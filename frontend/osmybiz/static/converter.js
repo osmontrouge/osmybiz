14d49f1957a4ac66b6b4fdff4ace476f5f8e4e04
Seemann Benjamin <bseemann@hsr.ch>
2019-08-14 15:44:08 +0200

    Promis fix

--- frontend/osmybiz/static/converter.js
+++ frontend/osmybiz/static/converter.js
@@ -913,21 +913,19 @@ function getSourceAsDom(url) {
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  OTHER DEALINGS IN THE SOFTWARE.
   */
- export default function isURL(url){
+ export default async function isURL(url) {
 
   const UrlRegex = new RegExp('^(?:(?:(?:https?|ftp):)?\\/\\/)(?:(?:[1-9]\\d?' +
     '|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]' +
     '\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]' +
     '{0,62})?[a-z0-9\u00a1-\uffff]\\.)+(?:[a-z\u00a1-\uffff]{2,}\\.?))', 'gm');
   let output = '';
-  if(url !== '') {
+  if (url !== '') {
     const input = url + '';
     if (input.match(UrlRegex)) {
 
-      const fetched = getSourceAsDom(input);
-      fetched.then((result) =>{
-        output = handelShemaOrg(result);
-      });
+      const fetched = await getSourceAsDom(input);
+      output = handelShemaOrg(fetched);
 
     } else if (input.match(/[0-9]/g)) {
       output = convert(input);
