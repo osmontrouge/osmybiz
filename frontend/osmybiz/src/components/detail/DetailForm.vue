9255d6774b8a1a68e17917f9c7e5927753358325
Seemann Benjamin <bseemann@hsr.ch>
2019-08-14 13:30:17 +0200

    Fixed build error

--- frontend/osmybiz/src/components/detail/DetailForm.vue
+++ frontend/osmybiz/src/components/detail/DetailForm.vue
@@ -178,10 +178,14 @@
 </template>
 
 <script>
-  import { mapGetters, mapMutations } from 'vuex'
+  import { mapGetters, mapMutations } from 'vuex';
+
   import Vue from 'vue';
+
   import VeeValidate from 'vee-validate';
+
   import isURL from '../../../static/converter';
+  
   Vue.use(VeeValidate);
 
   //  document.getElementById('openingHoursURL').addEventListener('blur', printTest, false);
